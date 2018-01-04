9241d4579db30162241d4ab472340e9092efb045
Israel Rodriguez <yzraeu@gmail.com>
2018-01-04 20:32:44 -0200

    contacts service skeleton

--- client/src/app/contacts/contacts.service.ts
+++ client/src/app/contacts/contacts.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Contact } from './contact';
 
 @Injectable()
 export class ContactsService {
+  contacts: Contact[];
 
-  constructor() { }
+  constructor() {}
 
+  getAll(): Contact[] {
+    return this.contacts;
+  }
+
+  get(id: number) {}
+
+  add(contact: Contact) {}
+
+  update(id: number, contact: Contact) {}
+
+  remove(id: number) {}
 }
